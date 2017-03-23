74762bd85c08e2ba7529b706f013dbb3edefac38
OpsMx <accounts@opsmx.com>
2017-03-23 15:49:16 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 echo "*********Downloading OpsMx Tcollector *********"
-sudo wget -O /tmp/tcollector_opsmx.tar https://github.com/OpsMx/tcollector/archive/master.zip
-sudo tar -xvf /tmp/tcollector_opsmx.tar -C /opt/
+sudo wget -O /tmp/tcollector_opsmx.zip https://github.com/OpsMx/tcollector/archive/master.zip
+sudo apt-get update && apt-get install unzip -y
+sudo unzip /tmp/tcollector_opsmx.zip -d /opt/
 sudo mv /opt/tcollector-master /opt/tcollector
 sudo wget -O /etc/init.d/tcollector https://raw.githubusercontent.com/OpsMx/tcollector/master/tcollector
-sudo rm -rf /tmp/tcollector_opsmx.tar
+sudo rm -rf /tmp/tcollector_opsmx.zip
 
 echo "********* Installing OpsMx Tcollector init Script **************"
 sudo chmod 755 /etc/init.d/tcollector
