9685e5849fa4916ce1fc0e7948da921cf85dc70e
OpsMx <accounts@opsmx.com>
2017-04-03 15:42:08 +0530

    Update installer.sh
    By default, it will start ifstat, iostat, procnettcp

--- installer.sh
+++ installer.sh
@@ -5,6 +5,8 @@ sudo wget -O /tmp/tcollector_opsmx.zip https://github.com/OpsMx/tcollector/archi
 sudo apt-get update && apt-get install unzip -y
 sudo unzip /tmp/tcollector_opsmx.zip -d /opt/
 sudo mv /opt/tcollector-master /opt/tcollector
+chmod -x /opt/tcollector/collectors/0/* /opt/tcollector/collectors/300/* /opt/tcollector/collectors/900/*
+chmod +x /opt/tcollector/collectors/0/ifstat.py /opt/tcollector/collectors/0/iostat.py /opt/tcollector/collectors/0/procnettcp.py
 sudo wget -O /etc/init.d/tcollector https://raw.githubusercontent.com/OpsMx/tcollector/master/tcollector
 sudo rm -rf /tmp/tcollector_opsmx.zip
 
