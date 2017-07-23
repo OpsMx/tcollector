eb9a2fcce3a5277cf3288e84e7b1269008360b2a
Veerendra <veerendra2@users.noreply.github.com>
2017-07-24 00:48:02 +0530

    Written from scratch, Added Network Metrics Also

--- collectors/0/docker.py
+++ collectors/0/docker.py
@@ -1,6 +1,8 @@
-#!/usr/bin/env python
-# More informations on https://docs.docker.com/articles/runmetrics/
-"""Imports Docker stats from /sys/fs/cgroup."""
+#!/usr/bin/python
+#
+# Author : OpsMx
+# Description : Retrieves Docker/Kubernetes container/POD Metrics
+# NOTE : Written from scratch
 
 import os
 import re
@@ -12,184 +14,181 @@ from collectors.etc import docker_conf
 from collectors.lib import utils
 from collectors.etc import opsmxconf
 
+
 CONFIG = docker_conf.get_config()
 
-CGROUP_PATH =CONFIG['cgroup_path']
-ENABLED = docker_conf.enabled()
-DOCKER_SOCK = CONFIG['socket_path']
-DOCKER_ROOT_DIR=CONFIG['docker_root_dir']
-
 if opsmxconf.OVERRIDE:
-    COLLECTION_INTERVAL=opsmxconf.GLOBAL_COLLECTORS_INTERVAL
+    COLLECTION_INTERVAL = opsmxconf.GLOBAL_COLLECTORS_INTERVAL
 else:
-    COLLECTION_INTERVAL=CONFIG['interval']
-
-#if not ENABLED:
-#  sys.stderr.write("Docker collector is not enabled")
-#  sys.exit(13)
-
-# proc_names example:
-# $ cat cpuacct.stat
-# user 58
-# system 72
-proc_names = {
-    "cpuacct.stat": (
-      "user", "system",
-    ),
-    "memory.stat": (
-        "cache", "rss", "mapped_file", "pgfault", "pgmajfault", "swap", "active_anon",
-        "inactive_anon", "active_file", "inactive_file", "unevictable",
-        "hierarchical_memory_limit", "hierarchical_memsw_limit",
-    ),
-}
-
-# proc_names_to_agg example:
-# $ cat blkio.io_service_bytes
-# 8:0 Read 8523776
-# 8:0 Write 1048576
-# ...
-# 8:1 Read 4223776
-# 8:1 Write 1042576
-# ...
-proc_names_to_agg = {
-    "blkio.io_service_bytes": (
-        "Read", "Write",
-    ),
-}
-
-def getnameandimage(containerid):
-    container_info=os.path.join(DOCKER_ROOT_DIR,"containers",containerid,"config.v2.json")
-    with open(container_info) as f:
-        data = json.load(f)
-    try:
-        containernames[containerid] = data["Name"].lstrip('/')
-    except:
-        print >>sys.stderr, containerid+" has no Name field"
-    try:
-        containerimages[containerid] = data["Config"]["Image"].replace(':', '_')
-    except:
-        print >>sys.stderr, containerid+" has no Image field"
+    COLLECTION_INTERVAL = CONFIG['interval']
 
 
-def senddata(datatosend, containerid):
-    if datatosend:
-        datatosend += " containerid="+containerid
-        if (containerid in containernames):
-            datatosend += " containername="+containernames[containerid]
-        if (containerid in containerimages):
-            datatosend += " containerimage="+containerimages[containerid]
-        print "docker.%s" % datatosend
-    sys.stdout.flush()
+CGROUP_PATH = "/sys/fs/cgroup"
+DOCKER_SOCK = "/var/run/docker.sock"
+DOCKER_DIR = "/var/lib/docker/containers"
+PROC_DIR = "/proc"
+COLLECTION_INTERVAL = 2
+MEMORY = {"cache": None, "rss": None,
+          "mapped_file": None, "pgfault": None,
+          "pgmajfault": None, "swap": None,
+          "active_anon": None, "inactive_anon": None,
+          "active_file": None, "inactive_file": None,
+          "unevictable": None, "hierarchical_memory_limit": None}
+CPU = {"user": None, "system" : None}
+BLKIO = {"Read": None, "Write" : None}
+container_id = re.compile(r'^[a-zA-Z0-9]{64}$')
 
-def readdockerstats(path, containerid):
-    #print "Path",path
-    #print "COntainer",containerid
-    # update containername and containerimage if needed
-    if ((containerid not in containernames) or (containerid not in containerimages)):
-        getnameandimage(containerid)
+for cpu_type in CPU.keys():
+    CPU[cpu_type] = re.compile(r'.*{}.*'.format(cpu_type))
+for blkio_type in BLKIO.keys():
+    BLKIO[blkio_type] = re.compile(r'.*{}.*'.format(blkio_type))
+for mem_type in MEMORY.keys():
+    MEMORY[mem_type] = re.compile(r'.*{}.*'.format(mem_type))
 
-    # Retrieve and push stats
-    for file_stat in os.listdir(path):
-        if (os.path.isfile(path+"/"+file_stat)\
-        and ((file_stat in proc_names.keys()) or (file_stat in proc_names_to_agg.keys()))):
-            try:
-                f_stat = open(path+"/"+file_stat)
-                #print "====>",path+"/"+file_stat
-            except IOError, e:
-                print >>sys.stderr, "Failed to open input file: %s" % (e,)
-                return 1
-            ts = int(time.time())
 
-            # proc_name
-            if (file_stat in proc_names.keys()):
-                datatosend = None
-                f_stat.seek(0)
-                for line in f_stat:
-                    tags = None
-                    subcattype = None
-                    fields = line.split()
-                    category = file_stat.split('.')[0]
-                    subcategory = fields[0]
-                    value = fields[1]
-                    if subcategory in proc_names[file_stat]:
-                        if category == 'memory':
-                            if subcategory in ['active_anon', 'inactive_anon']:
-                              subcattype = subcategory.split('_')[0]
-                              subcategory = 'anon'
-                            if subcategory in ['active_file', 'inactive_file']:
-                              subcattype = subcategory.split('_')[0]
-                              subcategory = 'file'
-                            tags = "type=%s" % subcategory
-                            if subcattype != None:
-                              tags += " subtype=%s" % subcattype
-                            datatosend = "%s %d %s %s" % (category, ts, value, tags)
-                        else:
-                            datatosend = "%s.%s %d %s" % (category, subcategory, ts, value)
-                        senddata(datatosend, containerid)
-            # proc_names_to_agg
+def get_net_stats(pid):  # Argument is 'list'
+    stats = {"rx_bytes": 0, "rx_pkt": 0,
+             "tx_bytes": 0, "tx_pkt": 0}
+    for ids in pid:
+        dev_file = os.path.join(PROC_DIR, str(ids), "net/dev")
+        if os.path.exists(dev_file):
+            with open(dev_file) as f:
+                for line in f.readlines():
+                    if re.search(r'Inter-', line) or re.search(r'face', line) or re.search(r'lo:', line):
+                        continue
+                    else:
+                        parts = line.split()
+                        stats["rx_bytes"] = stats["rx_bytes"] + int(parts[1])
+                        stats["rx_pkt"] = stats["rx_pkt"] + int(parts[2])
+                        stats["tx_bytes"] = stats["tx_bytes"] + int(parts[9])
+                        stats["tx_pkt"] = stats["tx_pkt"] + int(parts[10])
+        return stats
+
+
+class PreChecks:
+    def __init__(self):
+        self.config = dict()
+        if os.path.exists(os.path.join(CGROUP_PATH, "blkio/kubepods")):
+            self.type = "kubepods"
+        elif os.path.exists(os.path.join(CGROUP_PATH, "blkio/docker")):
+            self.type = "docker"
+        else:
+            print >> sys.stderr, "Not able to find pseudo files directory. Check docker daemon is running? or any check any \
+                    containers are running?"
+            exit(1)
+
+    def find_dir_list(self):
+        cpu_cpuacct = os.path.join(CGROUP_PATH, "cpu,cpuacct", self.type)
+        cpuacct = os.path.join(CGROUP_PATH, "cpuacct", self.type)
+        blkio = os.path.join(CGROUP_PATH, "blkio", self.type)
+        memory = os.path.join(CGROUP_PATH, "memory", self.type)
+        if os.path.exists(cpu_cpuacct):
+            self.config.setdefault("cpuacct", cpu_cpuacct)
+        elif os.path.exists(cpuacct):
+            self.config.setdefault("cpuacct", cpuacct)
+        if os.path.exists(blkio):
+            self.config.setdefault("blkio", blkio)
+        if os.path.exists(memory):
+            self.config.setdefault("memory", memory)
+        # Add 'kubepods' to paths
+        if self.type == "kubepods":
+            for key, item in self.config.items():
+                besteffort_path = os.path.join(item, "besteffort")
+                burstable_path = os.path.join(item, "burstable")
+                if os.path.exists(besteffort_path):
+                    self.config[key+"-besteffort"] = besteffort_path
+                if os.path.exists(burstable_path):
+                    self.config[key+"-burstable"] = burstable_path
+                del self.config[key]
+
+
+def get_info(path, ids):  # 'ids' is list type
+    global container_id
+    container_pids = list()
+    name = None
+    entity_id = None
+    if ids is None:
+        ids = filter(container_id.match, os.listdir(path))
+        is_kube = True
+    else:
+        ids = [ids]
+        is_kube = False
+    for cid in ids:
+        id_path = os.path.join(DOCKER_DIR, cid, "config.v2.json")
+        if os.path.exists(id_path):
+            with open(id_path) as f:
+                data = json.load(f)
+            pid = data["State"]["Pid"]
+            if is_kube:
+                try:
+                    name = data["Config"]["Labels"]["io.kubernetes.pod.name"]
+                    entity_id = data["Config"]["Labels"]["io.kubernetes.pod.uid"]
+                except:
+                    name = None
+                    entity_id = None
             else:
-                if (file_stat in proc_names_to_agg.keys()):
-                    for field_to_match in proc_names_to_agg[file_stat]:
-                        datatosend = None
-                        f_stat.seek(0)
-                        count = 0
-                        for line in f_stat:
-                            fields = line.split()
-                            if fields[1] == field_to_match:
-                                datatosend = "%s.%s" % (file_stat, fields[1].lower())
-                                try:
-                                    count += int(fields[2])
-                                except:
-                                    pass
-                        if datatosend:
-                            senddata("%s %d %s" % (datatosend, ts, count), containerid)
-            f_stat.close()
+                name = data["Name"].lstrip('/')
+                entity_id = data["ID"]
+            container_pids.append(pid)
+    return (str(entity_id), (str(name), container_pids))
 
-def main():
-    """docker_cpu main loop"""
-    global containernames
-    global containerimages
-    #utils.drop_privileges()
-    cache=0
-    while True:
 
-        # Connect to Docker socket to get informations about containers every 4 times
-        if (cache == 0):
-            containernames={}
-            containerimages={}
-        cache += 1
-        if (cache == 4):
-            cache = 0
+def display_metrics(metric_type, ts, metric_obj, stats_file, info, docker_type):
+    with open(stats_file) as f:
+        lines = f.readlines()
+    for sub_cat, obj in metric_obj.items():
+        try:
+            metric = filter(obj.match, lines)[0]
+        except:
+            continue
+        print "docker.{}.{} {} {} id={} name={} type={}".format(metric_type, sub_cat.lower(), ts, metric.split()[-1], info[0], info[1][0], docker_type)
 
-        if os.path.isdir(CGROUP_PATH):
-            for level1 in os.listdir(CGROUP_PATH):
-                #print "PATH====>",level1
-                if (os.path.isdir(CGROUP_PATH + "/"+level1+"/docker")): #\
-                # /cgroup/cpu and /cgroup/cpuacct are often links to /cgroup/cpu,cpuacct
-                #and not (((level1 == "cpu,cpuacct") or (level1 == "cpuacct")) and (os.path.isdir(CGROUP_PATH + "/cpu/docker")))):
-                    for level2 in os.listdir(CGROUP_PATH + "/"+level1+"/docker"):
-                        if os.path.isdir(CGROUP_PATH + "/"+level1+"/docker/"+level2):
-                            #print "LEVEL1",level1
-                            #print "LEVEL2",level2
-                            readdockerstats(CGROUP_PATH + "/"+level1+"/docker/"+level2, level2)
-                else:
-                    # If Docker cgroup is handled by slice
-                    # http://www.freedesktop.org/software/systemd/man/systemd.slice.html
-                    for slicename in ("system.slice", "machine.slice", "user.slice"):
-                        if (os.path.isdir(CGROUP_PATH + "/"+level1+"/"+slicename)\
-                        # /cgroup/cpu and /cgroup/cpuacct are often links to /cgroup/cpu,cpuacct
-                        and not (((level1 == "cpu,cpuacct") or (level1 == "cpuacct")) and (os.path.isdir(CGROUP_PATH + "/cpu/"+slicename)))):
-                            for level2 in os.listdir(CGROUP_PATH + "/"+level1+"/"+slicename):
-                                if os.path.isdir(CGROUP_PATH + "/"+level1+"/"+slicename+"/"+level2):
-                                    m = re.search("^docker-(\w+)\.scope$", level2)
-                                    if m:
-                                        readdockerstats(CGROUP_PATH + "/"+level1+"/"+slicename+"/"+level2, m.group(1))
-                                        break
-        if os.path.isdir(CGROUP_PATH + "/lxc"):
-            for level1 in os.listdir(CGROUP_PATH + "/lxc"):
-                if os.path.isdir(CGROUP_PATH + "/lxc/"+level1):
-                    readdockerstats(CGROUP_PATH + "/lxc/"+level1, level1)
+
+def main(config, docker_type):
+    cid = None
+    while 1:
+        ts = int(time.time())
+        all_pods_info = dict()
+        for key, path in config.items():
+            if "cpuacct" in key:
+                for dirs in os.listdir(path):
+                    cpu_file = os.path.join(path, dirs, "cpuacct.stat")
+                    if os.path.exists(cpu_file):
+                        if docker_type == "docker":
+                            cid = dirs
+                        info = get_info(os.path.join(path, dirs), cid)
+                        all_pods_info.setdefault(info[0], info[1])
+                        display_metrics("cpu", ts, CPU, cpu_file, info, docker_type)
+
+            if "memory" in key:
+                for dirs in os.listdir(path):
+                    memory_file = os.path.join(path, dirs, "memory.stat")
+                    if os.path.exists(memory_file):
+                        if docker_type == "docker":
+                            cid = dirs
+                        info = get_info(os.path.join(path, dirs), cid)
+                        all_pods_info.setdefault(info[0], info[1])
+                        display_metrics("memory", ts, MEMORY, memory_file, info, docker_type)
+
+            if "blkio" in key:
+                for dirs in os.listdir(path):
+                    blkio_file = os.path.join(path, dirs, "blkio.throttle.io_serviced")
+                    if os.path.exists(blkio_file):
+                        if docker_type == "docker":
+                            cid = dirs
+                        info = get_info(os.path.join(path, dirs), cid)
+                        all_pods_info.setdefault(info[0], info[1])
+                        display_metrics("blkio", ts, BLKIO, blkio_file, info, docker_type)
+
+        for ids, info in all_pods_info.items():
+            net_stat = get_net_stats(info[1])
+            for name, values in net_stat.items():
+                print "docker.net.{} {} {} id={} name={} type={}".format(name, ts, values, ids, info[0], docker_type)
         time.sleep(COLLECTION_INTERVAL)
+        sys.stdout.flush()
+
 
 if __name__ == "__main__":
-    sys.exit(main())
+    check = PreChecks()
+    check.find_dir_list()
+    main(check.config, check.type)
